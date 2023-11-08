64ca504280598445b29b287d3e6604f31a1ebc43
KadirEfeBircanoglu <kadirbrcnoglu147@gmail.com>
2023-11-08 16:56:13 +0300

    https://github.com/KadirBircanoglu/KaOsPizza/issues/3 Identity tabloları oluşturuldu

---
+++ KaOsPizzaEL/IdentityModels/AppUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaOsPizzaEL.IdentityModels
+{
+    public class AppUser : IdentityUser
+    {
+        [StringLength(50)]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [StringLength(50, MinimumLength = 2)]
+        public string Surname { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? InsertedDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
