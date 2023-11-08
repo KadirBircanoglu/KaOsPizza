f400e9b0b4c54bebc50aacf464340a38e2183088
KadirEfeBircanoglu <kadirbrcnoglu147@gmail.com>
2023-11-08 16:22:04 +0300

    Add project files.

---
+++ KaOsPizzaPL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KaOsPizzaPL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
