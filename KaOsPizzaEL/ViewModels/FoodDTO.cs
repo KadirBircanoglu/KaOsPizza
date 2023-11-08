86324bffe2b1663c984f27045f65a1b0d88949ba
OSMAN MURAT <192516036@ogr.cbu.edu.tr>
2023-11-08 19:26:39 +0300

    https://github.com/KadirBircanoglu/KaOsPizza/issues/5 DTO'lar oluşturuldu

---
+++ KaOsPizzaEL/ViewModels/FoodDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaOsPizzaEL.ViewModels
+{
+    public class FoodDTO
+    {
+        public long Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string PhotoLink { get; set; }
+        public long FoodTypeId { get; set; }
+        public string FoodMetarials { get; set; }
+    }
+}
---
+++ KaOsPizzaEL/ViewModels/FoodTypeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaOsPizzaEL.ViewModels
+{
+    public class FoodTypeDTO
+    {
+        public long Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Name { get; set; }
+    }
+}
