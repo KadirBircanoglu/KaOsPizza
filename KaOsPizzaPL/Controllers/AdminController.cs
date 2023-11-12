450c20370bb0fd8ff5dae75fac0156574ca802f8
KadirEfeBircanoglu <kadirbrcnoglu147@gmail.com>
2023-11-12 17:58:25 +0300

    https://github.com/KadirBircanoglu/KaOsPizza/issues/18 Admin panel devam edilecek

---
+++ KaOsPizzaPL/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KaOsPizzaPL.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddFood()
+        {
+            return View();
+        }
+    }
+}
