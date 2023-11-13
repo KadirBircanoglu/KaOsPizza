35b40a9add8cef84407a7207d30c41073e9aaf10
OSMAN MURAT <192516036@ogr.cbu.edu.tr>
2023-11-14 00:47:50 +0300

    https://github.com/KadirBircanoglu/KaOsPizza/issues/20 Servis sayfası tamamlandı

--- KaOsPizzaEL/Entities/Services.cs
+++ KaOsPizzaEL/Entities/Services.cs
@@ -13,5 +13,6 @@ namespace KaOsPizzaEL.Entities
         public string Name { get; set; }
         [StringLength(500)]
         public string? Description { get; set; }
+        public string FotoURL { get; set; }
     }
 }
